df99632a82451a0838e64b4acb564d071c0fa45b
niceholholl <165501701+niceholholl@users.noreply.github.com>
2025-12-06 03:22:37 +0900

    Update global_utils.py

--- network_tool_pkg/utils/global_utils.py
+++ network_tool_pkg/utils/global_utils.py
@@ -1,1 +1,8 @@
+import networkx as nx
 
+# -------------------- 전역 지표 계산 함수 : CC(클러스터링 계수), APL(평균 경로 길이), DIAM(지름) --------------------
+def calculate_global(G) :
+
+  # ---------- 네트워크 유효성 검사 ----------
+  if not isinstance(G, nx.Graph) :
+    raise TypeError('입력한 네트워크는 networkx.Graph 형태여야 합니다')
