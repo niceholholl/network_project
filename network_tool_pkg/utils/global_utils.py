327e3f32d9952bfa2dea30ca7f1034c80c18912f
niceholholl <165501701+niceholholl@users.noreply.github.com>
2025-12-06 03:27:01 +0900

    Update global_utils.py

--- network_tool_pkg/utils/global_utils.py
+++ network_tool_pkg/utils/global_utils.py
@@ -4,5 +4,20 @@ import networkx as nx
 def calculate_global(G) :
 
   # ---------- 네트워크 유효성 검사 ----------
+  
   if not isinstance(G, nx.Graph) :
-    raise TypeError('입력한 네트워크는 networkx.Graph 형태여야 합니다')
+    raise TypeError('입력한 네트워크의 형태가 올바르지 않습니다. networkx.Graph 형태로 입력하십시오.')
+
+  if len(G.nodes()) <= 1 :
+    raise ValueError('전역 지표 계산이 불가합니다. 노드 수가 1 이하입니다.')
+
+  if len(G.edges()) == 0 :
+    raise ValueError('전역 지표 계산이 불가합니다. 엣지가 존재하지 않습니다.')
+
+  # ---------- 클러스터링 계수 ----------
+  
+  cc = nx.average_clustering(G)
+
+  # ---------- 평균 경로 길이, 지름 ----------
+
+  #
