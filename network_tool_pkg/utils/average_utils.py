2a87c85fc05c8b6518541525a7474ce3b2daf26d
niceholholl <165501701+niceholholl@users.noreply.github.com>
2025-12-06 02:16:40 +0900

    Update average_utils.py

--- network_tool_pkg/utils/average_utils.py
+++ network_tool_pkg/utils/average_utils.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-# -------------------- 중심성 앙상블 평균화 함수 : 중심성 지표 결과를 노드별로 평균화하는 함수 --------------------
-def average_centrality(results_list) :
+# -------------------- 앙상블 평균화 함수 : 중심성 및 전역 지표 결과를 노드 및 지표별로 평균화하는 함수 --------------------
+def ensemble_average(results_list) :
 
   # ---------- 입력 유효성 및 리스트 형태 확인 (예외 처리) ----------
 
   if not isinstance(results_list, list) :
-    raise TypeError('average_centrality 함수의 입력은 리스트 형태여야 합니다.')
+    raise TypeError('ensemble_average 함수의 입력은 리스트 형태여야 합니다.')
 
   if not results_list :
-    raise ValueError('average_centrality 함수의 입력이 비어있습니다. 시뮬레이션 결과가 없습니다.')
+    raise ValueError('ensemble_average 함수의 입력이 비어있습니다. 시뮬레이션 결과가 없습니다.')
 
   # ---------- 리스트 내부의 딕셔너리를 Numpy 배열로 변환 ---------- 
 
@@ -17,8 +17,8 @@ def average_centrality(results_list) :
     values_array = np.array([list(dict(sorted(result.items())).values()) for result in results_list])
 
   except Exception as e :
-    raise RuntimeError('중심성 딕셔너리를 Numpy 배열로 변환하는 데 실패하였습니다. 오류 : {}'.format(e))
+    raise RuntimeError('Numpy 배열로 변환하는 데 실패하였습니다. 오류 : {}'.format(e))
   
-  # ---------- Numpy 배열 평균 계산 (노드별 평균) ----------
+  # ---------- Numpy 배열 평균 계산 (노드 및 지표별 평균) ----------
 
   return np.mean(values_array, axis = 0)
